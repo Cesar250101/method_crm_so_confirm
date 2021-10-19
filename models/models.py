3147650e6c3eada1ab4393e8979e9bd1719bd3d8
Cesar <cesar250101@outlook.com>
2021-10-19 12:00:16 -0300

    first commit

--- models/models.py
+++ models/models.py
@@ -9,7 +9,7 @@ class NotasVenta(models.Model):
     @api.multi
     def action_confirm(self):
         super(NotasVenta,self).action_confirm()
-        if self.self.opportunity_id.id:
+        if self.opportunity_id.id:
             oportunidad=self.env['crm.lead'].search([('id','=',self.opportunity_id.id)],limit=1)
             oportunidad.action_set_won_rainbowman()
             estado_ganado=self.env['crm.stage'].search([('name','=','Ganado')],limit=1)
