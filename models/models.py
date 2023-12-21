21659fa9d80dd07159b3dd205fac7f27d255f89f
cesar250101 <cesar250101@outlook.com>
2023-12-21 10:41:24 -0300

    Nose

--- models/models.py
+++ models/models.py
@@ -9,8 +9,8 @@ class NotasVenta(models.Model):
     @api.multi
     def action_confirm(self):
         super(NotasVenta,self).action_confirm()
-        if self.opportunity_id.id:
-            oportunidad=self.env['crm.lead'].search([('id','=',self.opportunity_id.id)],limit=1)
+        if self.opportunity_id:
+            oportunidad=self.opportunity_id
             oportunidad.action_set_won_rainbowman()
             estado_ganado=self.env['crm.stage'].search([('name','=','Ganado')],limit=1)
             oportunidad.stage_id= estado_ganado.id
